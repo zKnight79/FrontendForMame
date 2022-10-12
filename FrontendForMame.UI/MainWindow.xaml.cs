7747aaf79e99d4d3cac26632359eee8ec14524a6
Guillaume JIBAUT <guillaume.jibaut@live.com>
2022-10-12 22:39:18 +0200

    Added logo video fullname

--- FrontendForMame.UI/MainWindow.xaml.cs
+++ FrontendForMame.UI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Windows;
 
 namespace FrontendForMame.UI;
@@ -6,7 +8,7 @@ namespace FrontendForMame.UI;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow : Window, INotifyPropertyChanged
 {
     public MainWindow()
     {
@@ -16,6 +18,15 @@ public partial class MainWindow : Window
     public string Version { get; set; } = App.Version;
     public string Controller1Name { get; set; } = "Controller 1";
     public string Controller2Name { get; set; } = "Controller 2";
+    public string GameFullName { get; set; } = "Game Full Name";
+    public string GameLogoPath{ get; set; } = "/Data/Missing.png";
+    public string GameSnapPath { get; set; } = string.Empty;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string? name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
 
     private void Exit_Click(object sender, RoutedEventArgs e)
     {
