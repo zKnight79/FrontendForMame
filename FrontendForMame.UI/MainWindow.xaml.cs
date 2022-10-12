f127ca567e469d414a5e94e80ac72722aa7f78cd
Guillaume JIBAUT <guillaume.jibaut@konicaminolta.fr>
2022-10-12 16:07:16 +0200

    Added title and system buttons

--- FrontendForMame.UI/MainWindow.xaml.cs
+++ FrontendForMame.UI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 
 namespace FrontendForMame.UI;
 
@@ -10,5 +11,22 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Exit_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void Shutdown_Click(object sender, RoutedEventArgs e)
+    {
+        ProcessStartInfo psi = new()
+        {
+            FileName = "shutdown",
+            Arguments = "/s /t 0",
+            CreateNoWindow = true,
+            UseShellExecute = false
+        };
+        Process.Start(psi);
     }
 }
